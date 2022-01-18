6be9071a0982fa1f84c0f5032d9780dbadc56acd
Rivery <9858560+xigongdaEricyang@users.noreply.github.com>
2022-01-18 17:52:06 +0800

    chore: use dashboard image (#29)
    * chore: use dashboard image
    
    * chore: update package

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,8 +1,8 @@
 set -ex
 
 DIR=`pwd`
-DASHBOARD=$DIR/source/nebula-graph-dashboard
+DASHBOARD=$DIR/source/nebula-dashboard
 cd $DASHBOARD
 VERSION=`cat package.json | grep '"version":' | awk 'NR==1{print $2}' | awk -F'"' '{print $2}'`
 cd $DIR
-ossutil64 -e $1 -i $2 -k $3 -f cp ./  $4${VERSION} --include "nebula-graph-dashboard-*.tar.gz" --only-current-dir -r
+ossutil64 -e $1 -i $2 -k $3 -f cp ./  $4${VERSION} --include "nebula-dashboard-*.tar.gz" --only-current-dir -r
