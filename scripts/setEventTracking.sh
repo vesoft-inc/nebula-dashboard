ffac495f2d18dec2971698e4fd920b80daec4047
rivery <rivery.wang@vesoft.com>
2023-02-01 14:35:35 +0800

    chore: update event tracking

--- scripts/setEventTracking.sh
+++ scripts/setEventTracking.sh
@@ -1,1 +1,4 @@
-sed -i "s~<!-- Global Event Tracking -->~<script async src='https://www.googletagmanager.com/gtag/js?id=$1'></script>\n<script>\nwindow.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', '$1');\n</script>~g" ./src/index.html
+GH_ID=$1
+if [[ $GH_ID != "nightly" ]]; then
+  sed -i "s~<!-- Global Event Tracking -->~<script async src='https://www.googletagmanager.com/gtag/js?id=$GH_ID'></script>\n<script>\nwindow.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', '$GH_ID');\n</script>~g" ./src/index.html
+fi
