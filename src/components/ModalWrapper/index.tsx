9189f84b3138fac9b58416a1a5e00587601dc9d2
Miao Zhuang <1060950782@163.com>
2023-05-12 11:36:13 +0800

    fix: modal wrapper auto destroy (#258)
    * fix: modal wrapper auto destroy
    
    * fix: modal wrapper auto destroy

--- src/components/ModalWrapper/index.tsx
+++ src/components/ModalWrapper/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function ModalWrapper<T>(Component: React.FC<T>) {
+
   const container = document.createElement('div');
   const hide = () => {
     setTimeout(() => {
       ReactDOM.unmountComponentAtNode(container);
+      window.removeEventListener("popstate",hide); 
     }, 200);
   };
 
   const show = (props: T) => {
     ReactDOM.render(<Component visible {...props} hide={hide} />, container);
+    window.addEventListener("popstate",hide); 
   };
 
   return {
